581031e338665fee8e4fd6ff72dcc0e031914bed
shaheemMPM <shaheem.mpm@gmail.com>
2019-07-12 12:36:23 +0530

    Add Database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express    = require('express'),
       ejs        = require('ejs'),
-      bodyParser = require('body-parser');
+      bodyParser = require('body-parser'),
+      mongoose   = require('mongoose');
 
 app = express();
 
@@ -8,27 +9,47 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended : true}));
 
+mongoose.connect('mongodb://127.0.0.1/cars');
+
+let Schema = mongoose.Schema;
+
+var CarSchema = new Schema({
+  name    : String,
+  company : String,
+  type    : String,
+  image   : String
+});
+
+var cars_collection = mongoose.model('cars_collection', CarSchema);
+
 app.get('/', function(req, res) {
   res.redirect('/cars');
 });
 
-let dataList = [
-  {
-    'name'    : 'Hector',
-    'company' : 'MG',
-    'type'    : 'SUV',
-    'image'   : 'https://media.zigcdn.com/media/content/2019/Apr/mg-hector-0001_720x540.jpg'
-  },
-  {
-    'name'    : 'Huracan',
-    'company' : 'Lamborghini',
-    'type'    : 'SEDAN',
-    'image'   : 'https://photos7.motorcar.com/new-2019-lamborghini-huracan-performantespyder-8868-18368147-9-1024.jpg'
-  }
-];
+// let dataList = [
+//   {
+//     'name'    : 'Hector',
+//     'company' : 'MG',
+//     'type'    : 'SUV',
+//     'image'   : 'https://media.zigcdn.com/media/content/2019/Apr/mg-hector-0001_720x540.jpg'
+//   },
+//   {
+//     'name'    : 'Huracan',
+//     'company' : 'Lamborghini',
+//     'type'    : 'SEDAN',
+//     'image'   : 'https://photos7.motorcar.com/new-2019-lamborghini-huracan-performantespyder-8868-18368147-9-1024.jpg'
+//   }
+// ];
 
 app.get('/cars', function(req, res) {
-  res.render('cars', {'carsdata' : dataList});
+  cars_collection.find({}, function(err, quer) {
+    if (err) {
+      console.log("Error on find");
+      console.log(err);
+    }else {
+      res.render('cars', {'carsdata' : quer});
+    }
+  })
 });
 
 app.get('/addcar', function(req, res) {
@@ -37,8 +58,15 @@ app.get('/addcar', function(req, res) {
 
 app.post('/addcar', function(req, res) {
   // console.log(req.body);
-  dataList.push(req.body);
-  res.redirect('/cars');
+  // dataList.push(req.body);
+  cars_collection.create(req.body, function(err, query) {
+    if (err) {
+      console.log("Error on data entry");
+      console.log(err);
+    }else {
+      res.redirect('/cars');
+    }
+  })
 });
 
 let port = process.env.PORT || 8080;
